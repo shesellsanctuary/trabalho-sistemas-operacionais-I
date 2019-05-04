feb08427be98307f384725511727ce3666a164ed
Arthur Zachow <zcarthur@outlook.com>
2019-05-04 01:16:11 -0300

    fixed merge

--- tests/testCYield.c
+++ tests/testCYield.c
@@ -37,9 +37,8 @@ ETestStatus cyield_test()
     }
     else
     {
-        printf("exec thread: %d\n", ((TCB_t*)g_executingThread->first->node)->tid);
-        // First thread should have "apto" state
-        if (((TCB_t*)g_executingThread->first->node)->state != PROCST_APTO)
+        // x should have apto state
+        if (x->state != PROCST_APTO)
         {
             testStatus = TestError;
             perror("Wrong new state.\n");
