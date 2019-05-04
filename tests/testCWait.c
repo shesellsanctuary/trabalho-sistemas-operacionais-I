e54cd4d49bb8f9ad6b4750a7fff58fe00aa9e144
dbserver <emilycalvet.inf@gmail.com>
2019-05-04 16:43:33 -0300

    cwait testing

--- tests/testCWait.c
+++ tests/testCWait.c
@@ -11,7 +11,7 @@
 int dummyFunction()
 {
 	printf("dummy \n");
-	return 2;
+	return 0;
 }
 
 ETestStatus cwait_test()
@@ -34,21 +34,23 @@ ETestStatus cwait_test()
 	csem_init(pSem, c_count);
 
 	// Create 2 threads to compete for 1 resource
-	TCB_t* x = (TCB_t*)malloc(sizeof(TCB_t));
-	ucontext_t* context = (ucontext_t*)malloc(sizeof(ucontext_t));
-	getcontext(context);
-	x->context = *context;
-	x->prio = ThreadHighPriority;
-	x->state = PROCST_APTO;
-	x->tid = 1;
+	ccreate((void*)dummyFunction, NULL, ThreadHighPriority);
+	ccreate((void*)dummyFunction, NULL, ThreadHighPriority);
+	// TCB_t* x = (TCB_t*)malloc(sizeof(TCB_t));
+	// ucontext_t* context = (ucontext_t*)malloc(sizeof(ucontext_t));
+	// getcontext(context);
+	// x->context = *context;
+	// x->prio = ThreadHighPriority;
+	// x->state = PROCST_APTO;
+	// x->tid = 1;
 
-	appendThreadToReadyQueue(x);
+	// appendThreadToReadyQueue(x);
 
 	// Remove main thread from execution
 	cyield();
 	
-	TCB_t* thread = (TCB_t*)g_executingThread->first->node;
-	printf("executing: %d\n", thread->tid);
+	TCB_t* thread1 = (TCB_t*)g_executingThread->first->node;
+	printf("executing: %d\n", thread1->tid);
 
 	if (cwait(pSem) != OpSuccess)
 	{
@@ -58,28 +60,28 @@ ETestStatus cwait_test()
 	else
 	{
 		// First thread should be "executing", because it got the resource without waiting
-		if (x->state != PROCST_EXEC)
+		if (thread1->state != PROCST_EXEC)
 		{
 			TCB_t* thread = (TCB_t*)g_executingThread->first->node;
 			printf("executing: %d\n", thread->tid);
 			testStatus = TestError;
-			printf("fail state 1\n state: %d\n tid: %d\n", x->state, x->tid);
+			printf("fail state 1\n state: %d\n tid: %d\n", thread1->state, thread1->tid);
 		}
 		// Creating new thread and appending to ready queue
-		TCB_t* y = (TCB_t*)malloc(sizeof(TCB_t));
-		ucontext_t* context = (ucontext_t*)malloc(sizeof(ucontext_t));
-		getcontext(context);
-		y->context = *context;
-		y->prio = ThreadHighPriority;
-		y->state = PROCST_APTO;
-		y->tid = 2;
+		// TCB_t* y = (TCB_t*)malloc(sizeof(TCB_t));
+		// ucontext_t* context = (ucontext_t*)malloc(sizeof(ucontext_t));
+		// getcontext(context);
+		// y->context = *context;
+		// y->prio = ThreadHighPriority;
+		// y->state = PROCST_APTO;
+		// y->tid = 2;
 
-		appendThreadToReadyQueue(y);
+		// appendThreadToReadyQueue(y);
 
 		// Removing first thread from execution 
 		cyield();
-		TCB_t* thread = (TCB_t*)g_executingThread->first->node;
-		printf("executing: %d\n", thread->tid);
+		TCB_t* thread2 = (TCB_t*)g_executingThread->first->node;
+		printf("executing: %d\n", thread2->tid);
 		
 		// Requesting non existent resource
 		if (cwait(pSem) != OpSuccess)
@@ -90,17 +92,16 @@ ETestStatus cwait_test()
 		else
 		{
 			// Thread should be blocked and on both the resource and blocked queues
-			if ((SearchFila2(pSem->fila, y->tid) != OpSuccess) || 
-				(y->state != PROCST_BLOQ) || 
-				(SearchFila2(g_blockedQueue, y->tid)) != OpSuccess)
+			if ((SearchThreadFila2(pSem->fila, thread2->tid) != OpSuccess) || 
+				(thread2->state != PROCST_BLOQ) || 
+				(SearchThreadFila2(g_blockedQueue, thread2->tid)) != OpSuccess)
 			{
-				printf("second thread not on sem list\n tid: %d\n state: %d\n", y->tid, y->state);
+				printf("second thread not on sem list\n tid: %d\n state: %d\n", thread2->tid, thread2->state);
 				testStatus = TestError;
 
 			}
 		}
-		free(y);
-		free(context);
+		free(thread2);
 	}
 
 	// !!!  IMPORTANT !!!
@@ -112,7 +113,6 @@ ETestStatus cwait_test()
 	}
 	free(pSem->fila);
 	free(pSem);
-	free(context);
 
 	return testStatus;
 }
